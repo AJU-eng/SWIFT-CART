e28ce0b381230d740f7a4c45858c24fecb301ced
AJU-eng <aju33146@gmail.com>
2024-01-17 14:15:57 +0530

    cors changed

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const session=require('express-session');
 const router = require("./router/userRouter");
 const adminRouter = require("./router/adminRouter");
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"http://13.200.178.233",
     credentials:true
 }))
 app.use(cookie())
