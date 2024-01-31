93d5500edfa72cb9064ac24d97a07ecf6628c461
AJU-eng <aju33146@gmail.com>
2024-01-31 12:50:01 +0530

    projected

--- backend/controller/AddressController.js
+++ backend/controller/AddressController.js
@@ -10,7 +10,7 @@ const addAddress = async (req, res) => {
   );
   const { name, email, number, state, district, pincode, street } = req.body;
   const address = await AddressModel.findOne({ userId: user_id });
-  if (address ) {
+  if (address) {
     const data = await AddressModel.findOneAndUpdate(
       { userId: user_id },
       { $push: { Address: req.body } },
@@ -81,29 +81,34 @@ const findAddress = async (req, res) => {
 };
 
 const EditAddress = async (req, res) => {
-  const { name, email, number, district, state, pincode, street,id } = req.body;
+  const { name, email, number, district, state, pincode, street, id } =
+    req.body;
   const { user_id, iat } = jwt.decode(
     req.cookies.token,
     process.env.SECRET_KEY
   );
 
   const data = await AddressModel.findOneAndUpdate({
-    "Address._id":id
+    "Address._id": id,
   });
   console.log(data);
-  res.send(data)
+  res.send(data);
 };
 
-const getRecentAddress=async(req,res)=>{
+const getRecentAddress = async (req, res) => {
   console.log("recent");
-  const {user_id,iat}=jwt.decode(req.cookies.token,process.env.SECRET_KEY)
-  
-  const data=await OrderModel.findOne({userId:user_id})
-  const recentOrders=data.orders[data.orders.length-1]
-  // console.log(recentOrders.Address);
-  res.send(recentOrders.Address)
-}
+  const { user_id, iat } = jwt.decode(
+    req.cookies.token,
+    process.env.SECRET_KEY
+  );
 
+  const data = await OrderModel.findOne({ userId: user_id });
+  if (data) {
+    const recentOrders = data.orders[data.orders.length - 1];
+    // console.log(recentOrders.Address);
+    res.send(recentOrders.Address);
+  }
+};
 
 module.exports = {
   addAddress,
@@ -111,5 +116,5 @@ module.exports = {
   deleteAddress,
   findAddress,
   EditAddress,
-  getRecentAddress
+  getRecentAddress,
 };
--- backend/controller/returnController.js
+++ backend/controller/returnController.js
@@ -2,58 +2,88 @@ const returnModel = require("../model/OrderReturnModel");
 const jwt = require("jsonwebtoken");
 const WalletModel = require("../model/WalletModel");
 const userModel = require("../model/userModel");
+const OrderModel = require("../model/OrderModel");
 const returnRequest = async (req, res) => {
-  const {user_id,iat}=jwt.decode(req.cookies.token,process.env.SECRET_KEY)
-  const { name, price, reason } = req.body;
+  const { user_id, iat } = jwt.decode(
+    req.cookies.token,
+    process.env.SECRET_KEY
+  );
+  const { name, price, reason, _id } = req.body;
   const id = new Date().getTime();
   let obj = {
     productName: name,
     price: price,
     reason: reason,
     status: "requested",
+    productId: _id,
     id: id,
-    user:user_id
+    user: user_id,
   };
+
+  const data = await returnModel.create({ returns: obj });
+  // console.log(req.body);
+  // const update = await OrderModel.aggregate([
+  //   { $unwind: "$orders" },
+  //   { $unwind: "$orders.products" },
+  //   { $match: { "orders.products._id": _id } },
+  //   { $set: { "orders.products.return": "requested" } },
+  // ]);
+  // const update = await  OrderModel.updateOne(
+  //   { 
+  //      "orders.products._id": _id,
+  //      "userId": user_id
+  //   },
+  //   {
+  //      $set: { "orders.$[order].products.$[product].return": "requested" }
+  //   },
+  //   {
+  //      arrayFilters: [ { "order.products._id": _id}, { "product._id": _id } ]
+  //   }
+  // );
   
+ console.log(update);
  
-    const data = await returnModel.create({ returns: obj});
-    
+ 
 };
 
 const requestApprove = async (req, res) => {
   console.log(req.body);
- 
-  const { id,price,user} = req.body;
+
+  const { id, price, user } = req.body;
   const data = await returnModel.findOneAndUpdate(
     { "returns.id": id },
     { $set: { "returns.status": "approved" } },
     { new: true }
   );
   const date = new Date();
-  const _id=new Date().getTime()
+  const _id = new Date().getTime();
   let obj = {
     amount: price,
     type: "credit",
     Description: "Order Return Refund",
     Date: date,
-    id:_id
+    id: _id,
   };
   // console.log(data);
   const dataAvailable = await WalletModel.findOne({ userId: user });
   if (dataAvailable) {
     const data = await WalletModel.findOneAndUpdate(
       { userId: user },
-      { $push: { wallet: obj },$inc:{Balance:price} },
-      
-      {new:true}
+      { $push: { wallet: obj }, $inc: { Balance: price } },
+
+      { new: true }
     );
-    console.log(data,"updated");
+    console.log(data, "updated");
   } else {
-    const wallets = await WalletModel.create({ userId: user, wallet: obj ,Balance:price});
-    console.log(wallets+"created");
+    const wallets = await WalletModel.create({
+      userId: user,
+      wallet: obj,
+      Balance: price,
+    });
+    console.log(wallets + "created");
   }
-  console.log(data+"hei");
-  res.send(data)
+  console.log(data + "hei");
+  res.send(data);
 };
 
 const requestReject = async (req, res) => {
@@ -64,7 +94,7 @@ const requestReject = async (req, res) => {
     { new: true }
   );
   console.log(data);
-  res.send(data)
+  res.send(data);
 };
 
 const getRequest = async (req, res) => {
@@ -77,13 +107,11 @@ const singleReturn = async (req, res) => {
   // console.log(req.body);
   const { id } = req.body;
   // console.log(_id);
-  const returnRequest = await returnModel.findOne(
-    
-    { 'returns.$elemMatch': { 'id': id } }
-  );
-  console.log(returnRequest+'data');
+  const returnRequest = await returnModel.findOne({
+    "returns.$elemMatch": { id: id },
+  });
+  console.log(returnRequest + "data");
   // res.send(data)
-
 };
 module.exports = {
   returnRequest,
